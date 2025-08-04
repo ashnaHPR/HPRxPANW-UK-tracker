cab510b0a32ee1eb8f033d21f28dca6216740c73
ashnaHPR <ashna.syal@harvard.co.uk>
2025-08-04 17:19:43 +0100

    Update logger.py

--- .github/scripts/logger.py
+++ .github/scripts/logger.py
@@ -1,9 +1,10 @@
-# scripts/logger.py
-
 import logging
 
-logging.basicConfig(
-    format="%(asctime)s [%(levelname)s] %(message)s",
-    level=logging.INFO
-)
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
 
+console_handler = logging.StreamHandler()
+formatter = logging.Formatter("%(asctime)s [%(levelname)s] %(message)s")
+console_handler.setFormatter(formatter)
+
+logger.addHandler(console_handler)
